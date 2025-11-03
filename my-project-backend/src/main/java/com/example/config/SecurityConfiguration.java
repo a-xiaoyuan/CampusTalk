26c9e563b41130d5658ddf36f548d5efeb45f298
xiaoyu <2677941837@qq.com>
2025-11-03 20:15:10 +0800

    配置SpringSecurity基本内容

---
+++ my-project-backend/src/main/java/com/example/config/SecurityConfiguration.java
@@ -0,0 +1,62 @@
+package com.example.config;
+
+import com.example.entity.RestBean;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.io.IOException;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity  http)throws  Exception{
+        return http
+                .authorizeHttpRequests(conf->conf
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(conf->conf
+                        .loginProcessingUrl("/api/auth/login")
+                        .failureHandler(this::onAuthenticationFailure)
+                        .successHandler(this::onAuthenticationSuccess)
+                )
+                .logout(conf->conf
+                        .logoutUrl("/api/auth/logout")
+                        .logoutSuccessHandler(this::onLogoutSuccess)
+                )
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(conf->conf
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+
+    private void onLogoutSuccess(HttpServletRequest request,
+                                 HttpServletResponse response,
+                                 Authentication authentication)throws IOException, ServletException {
+    }
+
+    private void onAuthenticationSuccess(HttpServletRequest request,
+                                         HttpServletResponse response,
+                                         Authentication authentication)throws IOException, ServletException {
+        response.setContentType("application/json;charset=utf-8");
+        response.getWriter().write(RestBean.success().asJsonString());
+    }
+
+    private void onAuthenticationFailure(HttpServletRequest request,
+                                         HttpServletResponse response,
+                                         AuthenticationException exception)throws IOException, ServletException {
+        response.setContentType("application/json;charset=utf-8");
+        response.getWriter().write(RestBean.failure(401,exception.getMessage()).asJsonString());
+    }
+}
