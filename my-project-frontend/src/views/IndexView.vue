449c88d842fdb76c44e9caf227181fb568b6e4c5
xiaoyu <2677941837@qq.com>
2025-11-18 12:38:27 +0800

    首页顶部

--- my-project-frontend/src/views/IndexView.vue
+++ my-project-frontend/src/views/IndexView.vue
@@ -27,19 +27,63 @@ function userLogout(){
 </script>
 
 <template>
-  <!-- 主页面容器 -->
-  <div>
-    <!-- 
-      Element Plus按钮组件
-      点击触发用户注销操作
-    -->
-    <el-button @click="userLogout">退出登录</el-button>
+  <div class="main-content">
+    <el-container style="height: 100%">
+      <el-header class="main-content-header">
+        <el-image class="logo" src="https://element-plus.org/images/element-plus-logo.svg"></el-image>
+        <div style="flex:1" class="user-info" >
+          <div class="profile">
+            <div>用户名</div>
+            <div>电子邮箱</div>
+          </div>
+          <el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"></el-avatar>
+        </div>
+      </el-header>
+      <el-container>
+          <el-aside>Aside</el-aside>
+          <el-main>Main</el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
-<style scoped>
+<style lang="less" scoped>
 /**
  * 主页面样式区域
  * 当前页面样式为空，可根据需要添加样式
  */
+.main-content{
+    width: 100vw;
+    height: 100vh;
+}
+.main-content-header{
+    border-bottom: solid 1px var(--el-border-color);
+    height: 55px;
+    display: flex;
+    align-items: center;
+    box-sizing: border-box;
+
+    .logo{
+      height: 32px;
+    }
+    .user-info{
+      display: flex;
+      align-items: center;
+      justify-content: flex-end;
+    }
+    .profile {
+      text-align: right;
+      margin-right: 20px;
+    }
+    :first-child{
+      font-size: 18px;
+      font-weight: bold;
+      line-height: 20px;
+    }
+    :last-child{
+      font-size: 10px;
+      color: gray;
+    }
+
+}
 </style>
